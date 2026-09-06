38231d0ca7a92ef877ed5bd841ef46b7c65d7d2d
Adwaitb-112 <adwaitb11202@gmail.com>
2026-09-06 20:42:20 +0530

    Update CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const server = http.createServer(app)
 const port = process.env.PORT || 5000
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://food2u.onrender.com",
         credentials: true,
         methods: ['POST', 'GET']
     }
@@ -28,7 +28,7 @@ const io = new Server(server, {
 app.set("io", io)
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://food2u.onrender.com",
     credentials: true
 }))
 app.use(express.json())
